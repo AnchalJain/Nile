0ef975a0d59d11c129b719d545f1cc2b5c7002dd
AnchalJain <anchalj109@gmail.com>
2017-01-03 13:50:42 +0530

    add header

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+<body>
+<header role="banner">
+  <img id="logo-main" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32877/logo-thing.png" width="200" alt="Logo Thing main logo">
+<nav id="navbar-primary" class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><circle>Toggle navigation</circle></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <hr>
+    <div class="collapse navbar-collapse" id="navbar-primary-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#"><b>HOME</b></a></li>
+        <li><a href="#"><b>PRODUCTS</b></a></li>
+        <li><a href="#"><b>BLOG</b></a></li>
+        <li><a href="#"><b>CONTACTS</b></a></li>
+        <li><a href="#"><b>ABOUT</b></a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+   <hr> 
+  </div><!-- /.container-fluid -->
+</nav>
+</header><!-- header role="banner" -->
+</body>
+</html>
+
+
